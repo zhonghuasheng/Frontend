5622315b180aa39736f905a8b951c93269d10315
Luke Chen <luke.chen@augmentum.com>
2019-08-09 18:24:38 +0800

    Story #2 实现了登录的前端页面并美化

--- frontend/web/src/components/Login.vue
+++ frontend/web/src/components/Login.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="row">
-    <form class="col-12" @submit.prevent="loginAction" enctype="multipart/form-data">
-      <div class="form-group">
-          <label>用户名</label>
-          <input type="text" class="form-control" v-model="user.email" placeholder="请输入邮箱">
+  <div class='login'>
+    <el-form :model='user' :rules='rules' ref='loginForm'>
+      <el-form-item prop='email'>
+        <el-input v-model='user.email' auto-complete='on' placeholder='请输入邮箱'></el-input>
+      </el-form-item>
+      <el-form-item prop='password'>
+        <el-input v-model='user.password' type='password' auto-complete='on' placeholder='请输入密码'></el-input>
+      </el-form-item>
+      <div class='box clearfix'>
+        <span class='float-left' v-on:click='clearCookie'>忘记密码？</span>
+        <div class='float-right'>
+          <el-checkbox v-model='checked'>一周内自动登录</el-checkbox>
+        </div>
       </div>
-      <div class="form-group">
-        <label>密码</label>
-        <input type="password" class="form-control" v-model="user.password" placeholder="请输入密码">
-      </div>
-      <button type="submit" class="btn btn-primary">登录</button>
-    </form>
+      <el-form-item>
+          <el-button type='primary' class='w-100' v-on:click='loginAction(user)'>登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -22,23 +28,54 @@ export default {
       user: {
         email: '',
         password: ''
+      },
+      checked: false,
+      rules: {
+        email: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+          { type: 'email', message: '请输入正确的邮箱格式', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: blur },
+          { min: 6, max: 14, message: '密码长度在６到１４个字符', trigger: blur }
+        ]
       }
     }
   },
   methods: {
-    loginAction () {
-      var formData = new FormData()
-      formData.append('email', this.user.email)
-      formData.append('password', this.user.password)
+    loginAction (user) {
+      this.$refs['loginForm'].validate((valid) => {
+        if (valid) {
+          if (this.checked === true) {
+            this.setCookie(this.user.email, this.user.password, 7)
+          } else {
+            this.clearCookie()
+          }
 
-      this.axios.post('http://localhost:8081/user/login', formData).then((response) => {
-        console.log(response.data)
-        console.log('s')
-      }).catch((response) => {
-        console.log(response.data)
-        console.log('f')
+          this.axios.post('http://localhost:8081/user/login', user).then((response) => {
+            console.log(response.data)
+          }).catch((response) => {
+            console.log(response.data)
+          })
+        }
       })
+    },
+    setCookie (email, password, exdays) {
+      var exdate = new Date()
+      exdate.setTime(exdate.getTime() + 24 * 60 * 60 * 1000 * exdays)
+      window.document.cookie = 'email' + '=' + email + ';path=/;expires=' + exdate.toGMTString()
+      window.document.cookie = 'password' + '=' + password + ';path=/;expires=' + exdate.toGMTString()
+    },
+    clearCookie () {
+      this.setCookie('', '', -1)
     }
   }
 }
 </script>
+<style>
+.login {
+  min-width: 350px;
+  width: 25%;
+  margin: auto;
+}
+</style>
--- frontend/web/src/main.js
+++ frontend/web/src/main.js
@@ -1,6 +1,5 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Vue from 'vue'
 import App from './App'
@@ -10,11 +9,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-Vue.use(VueAxios, axios)
+import ElementUI from 'element-ui'
 
 Vue.config.productionTip = false
 
 // 这里在vue中引入element ui
+Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
 Vue.use(ElementUI)
 
 /* eslint-disable no-new */
